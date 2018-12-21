68b4d901176cac81a43e6983d20ee5bde5693984
Yuta Toyokawa <kppg42@gmail.com>
2018-12-21 14:24:11 +0900

    create blog reducer

---
+++ src/reducers/blog.js
@@ -0,0 +1,55 @@
+import { OrderedMap } from 'immutable';
+import { handleActions } from 'redux-actions';
+
+import BlogModel from '../models/Blog';
+import PostModel from '../models/Post';
+import rootActions from '../actions';
+
+const defaultState = {
+  blog: new BlogModel(),
+  error: null,
+  isFetching: false,
+};
+
+function getPostsFromApi(posts) {
+  const postModels = posts.map(
+    post =>
+      new PostModel({
+        id: post.sys.id,
+        title: post.fields.title,
+        body: post.fields.body,
+        createdAt: post.sys.createdAt,
+        updatedAt: post.sys.updatedAt,
+      })
+  );
+
+  const postsMap = postModels.reduce(
+    (map, post) => map.set(post.createdAt, post),
+    OrderedMap()
+  );
+  return postsMap;
+}
+
+export default handleActions(
+  {
+    [rootActions.requestBlog]: state => ({
+      ...state,
+      isFetching: true,
+    }),
+
+    [rootActions.successBlog]: (state, { payload }) => ({
+      ...state,
+      blog: getPostsFromApi(payload.blog.data.items),
+      error: null,
+      isFetching: false,
+    }),
+
+    [rootActions.failureBlog]: (state, { payload }) => ({
+      ...state,
+      blog: new BlogModel(),
+      error: payload,
+      isFetching: false,
+    }),
+  },
+  defaultState
+);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,7 @@
 import { combineReducers } from 'redux';
 import { connectRouter } from 'connected-react-router';
 
-export default history => combineReducers({ router: connectRouter(history) });
+import blogReducer from './blog';
+
+export default history =>
+  combineReducers({ router: connectRouter(history), blogReducer });
