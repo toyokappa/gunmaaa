d9fd5ac7914920edb58673d3a41c29e1ab8e04fc
Yuta Toyokawa <kppg42@gmail.com>
2018-12-22 22:55:50 +0900

    create saga for github

---
+++ src/sagas/github.js
@@ -0,0 +1,20 @@
+import { call, fork, put, take } from 'redux-saga/effects';
+
+import rootActions from '../actions';
+import * as githubApi from '../apis/githubApi';
+
+function* handleRequestGithub() {
+  while (true) {
+    yield take('REQUEST_GITHUB');
+    const { payload, error } = yield call(githubApi.getRepos);
+    if (payload && !error) {
+      yield put(rootActions.successGithub(payload));
+    } else {
+      yield put(rootActions.failureGithub(error));
+    }
+  }
+}
+
+export default function* rootSaga() {
+  yield fork(handleRequestGithub);
+}
